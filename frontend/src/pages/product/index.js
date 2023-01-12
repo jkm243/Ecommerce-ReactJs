35bc2e4fb47dcf351e82cab0b02353f1b074bf07
Jkm-Asus <jacqueskatsuva@gmail.com>
2023-01-12 10:47:04 +0300

    Product Screen

--- frontend/src/pages/product/index.js
+++ frontend/src/pages/product/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useReducer } from 'react'
+// import Rating from '../../components/Rating';
 import { useParams } from 'react-router-dom'
 
 const reducer = (state, action) => {
@@ -38,13 +39,49 @@ function Product() {
         fetchData();
     }, [slug]);
 
-    
+
     return (loading ? (
         <div className='App carte'>Loading....</div>
-    ):error ? (
+    ) : error ? (
         <div className='App carte'>{error}</div>
-    ): (
-        <div className='App carte'><h1>{product.name}</h1>
+    ) : (
+        <div className="item active">
+            <div className="single-slide-item slide1">
+                <div className="container">
+                    <div className="welcome-hero-content">
+                        <div className="row">
+                            <div className="col-sm-7">
+                                <div className="single-welcome-hero">
+                                    <div className="welcome-hero-img">
+                                        <img src={"../../" + (product.image)} alt="prod-img" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-5">
+                                <div className="single-welcome-hero">
+                                    <div className="welcome-hero-txt">
+                                        <h2>{product.name}</h2>
+                                        <p>
+                                            {product.description}
+                                        </p>
+                                        <div className="packages-price">
+                                            <p>
+                                                $ {product.price}
+                                                <del>$ 499.00</del>
+                                            </p>
+                                        </div>
+                                        <button className="btn-cart welcome-add-cart" >
+                                            <span className="lnr lnr-plus-circle"></span>
+                                            add <span>to</span> cart
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     ))
 }
