a600ed874e8442d3b1e61dc11b47060ea0e3a081
Jkm-Asus <jacqueskatsuva@gmail.com>
2023-01-23 07:28:49 +0300

    seedRoutes

--- backend/data.js
+++ backend/data.js
@@ -1,7 +1,7 @@
 const data = {
     products:[
         {
-            _id:'1',
+            // _id:'1',
             name:'Air Max 2015 Blue',
             slug:'air-max-2015-blue',
             category:'shoes',
@@ -16,7 +16,7 @@ const data = {
             vitrine:true
         },
         {
-            _id: '2',
+            // _id: '2',
             name:'Nike Air Force Vintage',
             slug:'nike-air-force-vintage',
             category:'shoes',
@@ -32,7 +32,7 @@ const data = {
             vitrine: true
         },
         {
-            _id: '3',
+            // _id: '3',
             name:'Air Force One Red',
             slug:'air-force-one-red',
             category:'shoes',
@@ -48,7 +48,7 @@ const data = {
             vitrine: true
         },
         {
-            _id: '4',
+            // _id: '4',
             name:'Nike Phylon 2021',
             slug:'nike-phylon',
             category:'shoes',
@@ -64,7 +64,7 @@ const data = {
             vitrine: false
         },
         {
-            _id: '5',
+            // _id: '5',
             name:'Air Max 2015',
             slug:'air-max-2015',
             category:'shoes',
@@ -80,7 +80,7 @@ const data = {
             vitrine: false
         },
         {
-            _id: '6',
+            // _id: '6',
             name:'Air Max King',
             slug:'air-max-king',
             category:'shoes',
@@ -96,7 +96,7 @@ const data = {
             vitrine: false
         },
         {
-            _id: '7',
+            // _id: '7',
             name:'Air Max Jordan',
             slug:'air-max-jordan',
             category:'shoes',
@@ -112,7 +112,7 @@ const data = {
             vitrine: false
         },
         {
-            _id: '8',
+            // _id: '8',
             name:'Nike Red Sneakers',
             slug:'nike-air-sneaker',
             category:'shoes',
