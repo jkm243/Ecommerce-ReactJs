18fd20b6dc7b23af7ef7462d1c88bfec3bc32d59
Jkm-Asus <jacqueskatsuva@gmail.com>
2023-03-10 03:15:29 +0300

    User-api ✅, But Add to cart ❌

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,7 +8,7 @@ productRouter.get('/', async (req, res) => {
     res.send(products);
 });
 
-productRouter.get('/api/products/slug/:slug', async (req, res) => {
+productRouter.get('/api/product/slug/:slug', async (req, res) => {
     const product = await Product.findOne({slug:req.params.slug});
     if (product) {
         res.send(product)
--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 
 dotenv.config();
-
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGODB_URI).then(()=>{
     console.log('connected to db')
 }).catch(err =>{
@@ -16,9 +17,12 @@ mongoose.connect(process.env.MONGODB_URI).then(()=>{
 
 
 const app = express();
-app.use('/api/seed',seedRouter)
-// test
-app.use('/api/products', productRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/seed',seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port,()=>{
