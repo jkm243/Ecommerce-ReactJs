9d1df607b61720744633b47eb242b470277b3a70
Jkm-Asus <jacqueskatsuva@gmail.com>
2023-03-15 07:34:06 +0300

    Update Package.json

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import data from './data.js';
+import path from 'path'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import seedRouter from './routes/seedRoutes.js';
@@ -30,7 +30,11 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
