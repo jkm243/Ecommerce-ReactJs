aba7e90cf7bb75e76fb6681f6b61fe1a06048f29
Jkm-Asus <jacqueskatsuva@gmail.com>
2023-03-15 06:26:24 +0300

    Order History finish

--- backend/server.js
+++ backend/server.js
@@ -10,18 +10,22 @@ import orderRouter from './routes/orderRoutes.js';
 
 dotenv.config();
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log('connected to db')
-}).catch(err =>{
+}).catch(err => {
     console.log(err.message)
-} )
+})
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/seed',seedRouter);
+app.get('/api/keys/paypal', (req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
+app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
@@ -29,6 +33,6 @@ app.use('/api/orders', orderRouter);
 
 
 const port = process.env.PORT || 5000;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`);
 })
